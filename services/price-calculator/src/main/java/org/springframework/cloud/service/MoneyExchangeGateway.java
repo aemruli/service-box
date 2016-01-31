4beac21c70bb47d0bee946f413dc7093f4f774ea
Agim Emruli <agimemruli@gmail.com>
2016-01-31 19:17:45 +0100

    Add feign based implementation

--- services/price-calculator/src/main/java/org/springframework/cloud/service/MoneyExchangeGateway.java
+++ services/price-calculator/src/main/java/org/springframework/cloud/service/MoneyExchangeGateway.java
@@ -1,8 +1,16 @@
 package org.springframework.cloud.service;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * @author Agim Emruli
  */
+@FeignClient("CURRENCY")
 public interface MoneyExchangeGateway {
-    Double exchangeMoney(String currency, long amount);
+
+    @RequestMapping(value = "/rate/{currency}/{amount}", method = RequestMethod.GET)
+    Double exchangeMoney(@PathVariable("currency") String currency, @PathVariable("amount") long amount);
 }
--- services/price-calculator/src/main/java/org/springframework/cloud/service/PriceCalculator.java
+++ services/price-calculator/src/main/java/org/springframework/cloud/service/PriceCalculator.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author Agim Emruli
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableFeignClients
 public class PriceCalculator {
 
     public static void main(String[] args) {
